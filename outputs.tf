bb926d0a579554b455ba44a316368086e3fceed5
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,25 +1,21 @@
-output "application_load_balancers" {
-  description = "All application_load_balancer resources"
-  value       = azurerm_application_load_balancer.application_load_balancers
-}
 output "application_load_balancers_location" {
-  description = "List of location values across all application_load_balancers"
-  value       = [for k, v in azurerm_application_load_balancer.application_load_balancers : v.location]
+  description = "Map of location values across all application_load_balancers, keyed the same as var.application_load_balancers"
+  value       = { for k, v in azurerm_application_load_balancer.application_load_balancers : k => v.location }
 }
 output "application_load_balancers_name" {
-  description = "List of name values across all application_load_balancers"
-  value       = [for k, v in azurerm_application_load_balancer.application_load_balancers : v.name]
+  description = "Map of name values across all application_load_balancers, keyed the same as var.application_load_balancers"
+  value       = { for k, v in azurerm_application_load_balancer.application_load_balancers : k => v.name }
 }
 output "application_load_balancers_primary_configuration_endpoint" {
-  description = "List of primary_configuration_endpoint values across all application_load_balancers"
-  value       = [for k, v in azurerm_application_load_balancer.application_load_balancers : v.primary_configuration_endpoint]
+  description = "Map of primary_configuration_endpoint values across all application_load_balancers, keyed the same as var.application_load_balancers"
+  value       = { for k, v in azurerm_application_load_balancer.application_load_balancers : k => v.primary_configuration_endpoint }
 }
 output "application_load_balancers_resource_group_name" {
-  description = "List of resource_group_name values across all application_load_balancers"
-  value       = [for k, v in azurerm_application_load_balancer.application_load_balancers : v.resource_group_name]
+  description = "Map of resource_group_name values across all application_load_balancers, keyed the same as var.application_load_balancers"
+  value       = { for k, v in azurerm_application_load_balancer.application_load_balancers : k => v.resource_group_name }
 }
 output "application_load_balancers_tags" {
-  description = "List of tags values across all application_load_balancers"
-  value       = [for k, v in azurerm_application_load_balancer.application_load_balancers : v.tags]
+  description = "Map of tags values across all application_load_balancers, keyed the same as var.application_load_balancers"
+  value       = { for k, v in azurerm_application_load_balancer.application_load_balancers : k => v.tags }
 }
 
